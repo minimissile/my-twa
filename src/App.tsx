f23fc6fe6712f09b9a5159e40c2bb070fb71568c
minimissile <3528816081@qq.com>
2024-07-29 15:11:23 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,67 @@
-import './App.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
-import { useTonConnect } from './hooks/useTonConnect';
-import { useCounterContract } from './hooks/useCounterContract';
+import './App.css'
+import { ConnectedWallet, TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react'
+import { useTonConnect } from './hooks/useTonConnect'
+import { useCounterContract } from './hooks/useCounterContract'
+import '@twa-dev/sdk'
+import { useEffect } from 'react'
 
 function App() {
-    const { connected } = useTonConnect();
-    const { value, address, sendIncrement } = useCounterContract();
+  const [tonConnectUI] = useTonConnectUI()
+  const { connected } = useTonConnect()
+  const { value, address, sendIncrement } = useCounterContract()
+  const { modal } = tonConnectUI
 
-    return (
-        <div className='App'>
-            <div className='Container'>
-                <TonConnectButton />
+  useEffect(() => {
+    tonConnectUI.onStatusChange((wallet: any) => {
+      console.log('walletsssss', wallet)
 
-                <div className='Card'>
-                    <b>Counter Address</b>
-                    <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
-                </div>
+      const tonProof = wallet.connectItems?.tonProof
+      console.log('tonProof', tonProof)
 
-                <div className='Card'>
-                    <b>Counter Value</b>
-                    <div>{value ?? 'Loading...'}</div>
-                </div>
+      if (tonProof) {
+        if ('proof' in tonProof) {
+          // this.checkProof(tonProof.proof, wallet.account)
+          return
+        }
 
-                <a
-                    className={`Button ${connected ? 'Active' : 'Disabled'}`}
-                    onClick={() => {
-                        sendIncrement();
-                    }}
-                >
-                    Increment
-                </a>
-            </div>
+        console.error(tonProof.error)
+      }
+    })
+  }, [tonConnectUI])
+
+  const onSign = () => {
+    const msg = '123456'
+    console.log(tonConnectUI)
+  }
+
+  return (
+    <div className="App">
+      <div className="Container">
+        <p>1</p>
+        <TonConnectButton />
+
+        <div className="Card">
+          <b>Counter Address</b>
+          <div className="Hint">{address?.slice(0, 30) + '...'}</div>
         </div>
-    );
+
+        <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+          onClick={() => {
+            sendIncrement()
+          }}
+        >
+          Increment
+        </a>
+
+        <div className={'Button'} onClick={onSign}>
+          签名
+        </div>
+
+        <p>v 1.1.1</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
